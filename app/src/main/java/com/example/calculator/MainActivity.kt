aeb1b8cc3a3cd0e8ad5c249131329592fe3df548
Ronald Stivel Melo Cardenas <ronald9412@gmail.com>
2023-03-17 16:15:53 -0500

    Se modifico la interfaz principal

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,25 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.calculator.databinding.ActivityMainBinding
 import com.example.calculator.imcapp.ImcActivity
 import com.example.calculator.settingsapp.SettingsActivity
 import com.example.calculator.superheroapp.SuperHeroListActivity
 import com.example.calculator.todoapp.TodoActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val imcButton = findViewById<Button>(R.id.imc_button)
-        val todoButton = findViewById<Button>(R.id.TODO_button)
-        val superheroButton = findViewById<Button>(R.id.super_hero_button)
-        val settingsButton = findViewById<Button>(R.id.settings_button)
+        binding.apply {
+            imcButton.setOnClickListener { navigateToImcApp() }
+            TODOButton.setOnClickListener { navigateToTodoApp() }
+            superHeroButton.setOnClickListener { navigateToSuperheroApp() }
+            settingsButton.setOnClickListener { navigateToSettingsApp() }
+        }
 
-        imcButton.setOnClickListener{navigateToImcApp()}
-        todoButton.setOnClickListener { navigateToTodoApp() }
-        superheroButton.setOnClickListener { navigateToSuperheroApp() }
-        settingsButton.setOnClickListener { navigateToSettingsApp() }
     }
 
     private fun navigateToSuperheroApp() {
