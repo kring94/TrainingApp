017496cf87f3349fc742a47ad88ca70c9146cd2c
Ronald Stivel Melo Cardenas <ronald9412@gmail.com>
2023-04-12 11:36:29 -0500

    Se corrigio la asignación de nombre del NetworkModule de la app Horoscope

--- app/src/main/java/com/example/calculator/horoscopeapp/core/NetworkModule.kt
+++ app/src/main/java/com/example/calculator/horoscopeapp/core/NetworkModule.kt
@@ -7,21 +7,25 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object NetworkModule {
-//    @Provides
-//    @Singleton
-//    fun provideRetrofit():Retrofit = Retrofit
-//            .Builder()
-//            .baseUrl("https://aztro.sameerkumar.website")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//    @Provides
-//    fun provideHoroscopeApi(retrofit: Retrofit): HoroscopeApi{
-//        return retrofit.create(HoroscopeApi::class.java)
-//    }
-//}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    @Named("RetrofitHoroscope")
+    fun provideRetrofitHoroscope(): Retrofit = Retrofit
+            .Builder()
+            .baseUrl("https://aztro.sameerkumar.website")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Named("HoroscopeApi")
+    fun provideHoroscopeApi(retrofit: Retrofit): HoroscopeApi{
+        return retrofit.create(HoroscopeApi::class.java)
+    }
+}
